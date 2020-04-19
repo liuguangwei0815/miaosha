797326f9b7cb8853d438e92ca306db4443e67fb1
刘威 <736873792@qq.com>
2020-04-19 19:42:41 +0800

    fix

--- src/main/java/com/im/miaosha/MainApplication.java
+++ src/main/java/com/im/miaosha/MainApplication.java
@@ -1,5 +1,6 @@
 package com.im.miaosha;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.ResourceBanner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,6 +10,7 @@ import org.springframework.core.io.ClassPathResource;
  * @author liuwei
  */
 @SpringBootApplication
+@MapperScan("com.im.miaosha.dao")
 public class MainApplication {
     public static void main(String[] args) {
         SpringApplication springApplication = new SpringApplication(MainApplication.class);
